466e5157f0256e67bd256395d75b8c0c540dcf04
Talha Mohammed Khan <tallhakhan5432@example.com>
2025-07-26 23:59:27 +0530

    Url fixes

--- note-app-client/src/services/api.js
+++ note-app-client/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: process.env.REACT_APP_BACKEND_URL,
+  withCredentials: true,
 });
+
 
 export default api;
