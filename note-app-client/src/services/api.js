80b24df0a8eebfd431a380c814fd31153eba7aff
Talha Mohammed Khan <tallhakhan5432@example.com>
2025-07-27 13:52:13 +0530

    🔧 Fix CORS: allow deployed frontends 11

--- note-app-client/src/services/api.js
+++ note-app-client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://notes-backend-7je9.onrender.com", // hardcoded for now
+  baseURL: "https://notes-backend-7je9.onrender.com/api", // hardcoded for now
   withCredentials: true,
 });
 
