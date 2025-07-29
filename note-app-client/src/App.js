6aa28ee2c3f6bfb19f78143be07f53c5efca7eef
Talha Mohammed Khan <tallhakhan5432@example.com>
2025-07-29 11:14:26 +0530

    fix: import Signup in App.js

--- note-app-client/src/App.js
+++ note-app-client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import VerifyOtp from "./pages/VerifyOtp";
 import Dashboard from "./pages/Dashboard";
 import GoogleSuccess from "./pages/GoogleSuccess";
